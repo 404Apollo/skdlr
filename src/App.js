801262e79e1b2cadcaee556a085f849d274eb3d9
Abdu <abodekayali@gmail.com>
2021-11-18 21:16:40 -0500

    FireBase Setup/Initialized

--- src/App.js
+++ src/App.js
@@ -3,21 +3,27 @@ import './App.css';
 
 function App() {
   return(
-    <div className="App">
-      <div className = "registeration">
+    <div className="App" >
+      <div className = "registeration" class="section">
         <h1>Registeration</h1>
         <label>Username</label>
+        <br/>
         <input type="text"/>
+        <br/>
         <label>Password</label>
+        <br/>
         <input type="text"/>
+        <br/>
         <button>Register</button>
       
       </div>
-      <div className="login">
+      <div className="login" class="section">
         <h1>Login</h1>
         <input type="text" placeholder="Username..."/>
+        <br/>
         <input type="text" placeholder="Password..."/>
-        <button> Register </button>
+        <br/>
+        <button> Login </button>
       </div>
     </div>
   )
@@ -25,23 +31,3 @@ function App() {
 }
 
 export default App;
-  
-
-// return (
-  //   <div className="App">
-  //     <header className="App-header">
-  //       <img src={logo} className="App-logo" alt="logo" />
-  //       <p>
-  //         Edit <code>src/App.js</code> and save to reload.
-  //       </p>
-  //       <a
-  //         className="App-link"
-  //         href="https://reactjs.org"
-  //         target="_blank"
-  //         rel="noopener noreferrer"
-  //       >
-  //         Learn React
-  //       </a>
-  //     </header>
-  //   </div>
-  // );
